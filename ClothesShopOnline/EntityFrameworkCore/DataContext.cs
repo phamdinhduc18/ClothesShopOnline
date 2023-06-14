5e85c01ca3b544ff1ced3f1b5541bdd3bde850f3
Duc Pham <admin@DESKTOP-1P2BVKM>
2023-06-14 18:31:30 +0700

    add db context

--- ClothesShopOnline/EntityFrameworkCore/DataContext.cs
+++ ClothesShopOnline/EntityFrameworkCore/DataContext.cs
@@ -4,12 +4,16 @@ namespace EntityFrameworkCore
 {
     public class DataContext : DbContext
     {
-        public DataContext()
+        protected readonly IConfiguration Configuration;
+        public DataContext(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // connect to sql server with connection string from app settings
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
